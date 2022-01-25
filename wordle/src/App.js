3e6ac8f4e5cbce5437226be1717ab3a2ca3bb263
saraiyakrs <718618@sylvaniastudents.org>
2022-01-25 13:25:55 -0500

    define newLetter state

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -4,13 +4,31 @@ import Board from './Board'
 
 
 function App() {
+<<<<<<< Updated upstream
+=======
+
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const alphaArr = alphabet
+    .split('')
+    .map(l => ({
+      letter: l,
+      color:"lightgrey"
+    }))
+  const [letters, setLetters] = useState(alphaArr)
+  const [newLetter, setNewLetter] = useState('') 
+>>>>>>> Stashed changes
   return (
     <div className="App">
       <header>
        <h1>Wordle</h1>
       </header>
+<<<<<<< Updated upstream
       <Board/>
       <Keyboard/>
+=======
+      <Board letters = {letters} setLetters={setLetters} newLetter = {newLetter}/>
+      <Keyboard letters = {letters} setNewLetter ={setNewLetter}/>
+>>>>>>> Stashed changes
     </div>
   );
 }
