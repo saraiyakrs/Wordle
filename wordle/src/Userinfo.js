b25e9aefe6f6909801db0289b9b4449290891791
saraiyakrs <718618@sylvaniastudents.org>
2022-01-27 14:21:42 -0500

    little css stuff

--- wordle/src/Userinfo.js
+++ wordle/src/Userinfo.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Userinfo = ({ username, bestScore, gamesBeaten, gamesPlayed }) => {
+    const [newUsername, setnewUsername] = useState('')
+    const handleUsername = () => {
+        setnewUsername(newUsername)
+    }
     return (
         <div>
             <h1>User Login</h1>
@@ -8,13 +12,13 @@ const Userinfo = ({ username, bestScore, gamesBeaten, gamesPlayed }) => {
                 <label for = "username"> Username </label>
                 <input type = "text" id = "username" name = "username"></input>
             </form>
-            <button>Enter</button>
+            <button onclick = {handleUsername}>Enter</button>
             <h2>User Info</h2>
-            <h2> Username: {username}</h2>
+            <h3> Username: {username}</h3>
             
             <h3> Best Score: {bestScore}</h3>
-            <h4> Games Beaten: {gamesBeaten}</h4>
-            <h5> Games Played: {gamesPlayed} </h5>
+            <h3> Games Beaten: {gamesBeaten}</h3>
+            <h3> Games Played: {gamesPlayed} </h3>
             
         </div>
         
